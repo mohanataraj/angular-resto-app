552990e0046182a04eb6d92d5f0febf0f0f1a969
Mohan Nataraj <6969451+mohanataraj@users.noreply.github.com>
2021-07-29 17:08:31 +0000

    Angular components part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import  'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +33,10 @@ import { MenuComponent } from './menu/menu.component';
     FlexLayoutModule,
     MatToolbarModule,
     AppRoutingModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
